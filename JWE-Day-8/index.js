6bc5750745a7a93a61de002e9d93bd7f08747d92
Kusum <130838069+kusum-sahani@users.noreply.github.com>
2023-08-14 15:29:42 +0530

    Add files via upload

---
+++ JWE-Day-8/index.js
@@ -0,0 +1,11 @@
+// function selfPrint hello world
+    console.log("Hello World");
+
+// Calculate the sum
+function addNumbers(firstNumber, secondNumber) {
+       let sum = firstNumber + secondNumber;
+       return sum;
+}
+    
+let returnVal = sum(firstNumber, secondNumber);
+    console.log( returnVal);
